57450856f54fe0c18cea391df9c8dd8926283a98
mattditton <mattditton@351bf6ae-c542-0410-ac58-c1a849201edc>
2008-07-08 13:56:34 +0000

    Using the material in OPENGL. only hooked into the triangle mode. Very simple lighting setup. Will have to get the processing scene lights to do it properly. Next step textures.

--- trunk/OBJ_Loader/src/saito/objloader/Material.java
+++ trunk/OBJ_Loader/src/saito/objloader/Material.java
@@ -28,31 +28,42 @@ public class Material {
 	public String mtlName;
 	
 	public Material() {
-		Ka = new float[3];
-		Kd = new float[3];
-		Ks = new float[3];
+		Ka = new float[4];
+		Kd = new float[4];
+		Ks = new float[4];
 		for (int i = 0; i < Ka.length; i++) {
-			Ka[i] = 0.5f;
-			Kd[i] = 0.5f;
-			Ks[i] = 0.5f;
+			if(i == 3){
+				Ka[i] = 1f;
+				Kd[i] = 1f;
+				Ks[i] = 1f;
+			}
+			else{
+				Ka[i] = 0.5f;
+				Kd[i] = 0.5f;
+				Ks[i] = 0.5f;				
+			}
 		}
 		d = 1.0f;
 		
 		mtlName = "default";
 	}
 	
-	public void useMtlOPENGL(GL gl){
+	public void useMtlOPENGL(GL gl, Debug debug){
 		
+		//debug.println("Ambient");
         gl.glMaterialfv(GL.GL_FRONT_AND_BACK,GL.GL_AMBIENT,Ka,0);
         
 	    if(Kd[3]==1){
+	    	//debug.println("Diffuse");
 	    	gl.glMaterialfv(GL.GL_FRONT_AND_BACK,GL.GL_AMBIENT_AND_DIFFUSE,Kd,0);
+	    	
 	    }
 	    
 	    else{
+	    	//debug.println("Diffuse & Spec");
 			gl.glMaterialfv(GL.GL_FRONT_AND_BACK,GL.GL_AMBIENT_AND_DIFFUSE,Kd,0);
 		    gl.glMaterialfv(GL.GL_FRONT_AND_BACK,GL.GL_SPECULAR,Ks,0);
+		    
 	    }
-		
 	}
 }
