2f99f07c0dd90ba6df31e7f6ad2f37bdbfe77155
mattditton <mattditton@351bf6ae-c542-0410-ac58-c1a849201edc>
2008-10-14 22:01:42 +0000

    Update to OBJBoundingBox to fix the width height and depth. I must've been on crack.

--- OBJ_Loader/src/saito/objtools/OBJBoundingBox.java
+++ OBJ_Loader/src/saito/objtools/OBJBoundingBox.java
@@ -9,12 +9,7 @@ public class OBJBoundingBox  implements PConstants{
 	private float x1 = MAX_FLOAT,y1 = MAX_FLOAT,z1 = MAX_FLOAT,x2 = MIN_FLOAT,y2 = MIN_FLOAT,z2 = MIN_FLOAT;
 	private float centerX, centerY,centerZ;
 	public float width, height, depth;
-	
-	int fillColor;
-	int strokeColor;
-	
-	boolean fill;
-	boolean stroke;
+
 	
 	public OBJBoundingBox(PApplet parent, OBJModel model){
 		
@@ -37,23 +32,28 @@ public class OBJBoundingBox  implements PConstants{
 				
 				v = model.getVertex(i);
 				
-				x1 = PApplet.min(x1,v.vx);
-				y1 = PApplet.min(y1,v.vy);
-				z1 = PApplet.min(z1,v.vz);
+				x1 = Math.min(x1,v.vx);
+				y1 = Math.min(y1,v.vy);
+				z1 = Math.min(z1,v.vz);
 				
-				x2 = PApplet.max(x2,v.vx);
-				y2 = PApplet.max(y2,v.vy);
-				z2 = PApplet.max(z2,v.vz);
+				x2 = Math.max(x2,v.vx);
+				y2 = Math.max(y2,v.vy);
+				z2 = Math.max(z2,v.vz);
 				
 			}
 			
-			width =  Math.abs(x1) + Math.abs(x2);
-			height = Math.abs(y1) + Math.abs(y2);
-			depth =  Math.abs(z1) + Math.abs(z2);
+			width  = (float)Math.sqrt((x2 - x1) * (x2 - x1)); 
+			height = (float)Math.sqrt((y2 - y1) * (y2 - y1));
+			depth  = (float)Math.sqrt((z2 - z1) * (z2 - z1));
 			
-			centerX = x1 + (( x2 - x1 )/ 2);
-			centerY = y1 + (( y2 - y1 )/ 2);
-			centerZ = z1 + (( z2 - z1 )/ 2);
+			
+//			width =  Math.abs(x1) + Math.abs(x2);
+//			height = Math.abs(y1) + Math.abs(y2);
+//			depth =  Math.abs(z1) + Math.abs(z2);
+			
+			centerX = x1 + (width / 2);
+			centerY = y1 + (height/ 2);
+			centerZ = z1 + (depth / 2);
 		}
 	}
 	
@@ -61,28 +61,15 @@ public class OBJBoundingBox  implements PConstants{
 	
 	public void draw(){
 		
-//		fillColor = parent.g.fillColor;
-//		strokeColor = parent.g.strokeColor;
-//		
-//		fill = parent.g.fill;
-//		stroke = parent.g.stroke;
-//		
+	
 		parent.rectMode(CORNERS);
-		
-//		parent.noFill();
-//		parent.stroke(255,0,255);
-		
+			
 		parent.pushMatrix();
 		parent.translate(centerX, centerY, centerZ);
 	  
 		parent.box(width, height, depth);
 		parent.popMatrix();
-//		
-//		parent.fill(fillColor);
-//		parent.stroke(strokeColor);
-//		
-//		parent.g.fill = fill;
-//		parent.g.stroke = stroke;
+
 		
 	}
 	
