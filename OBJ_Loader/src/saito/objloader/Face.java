a4589333d7ac6a75f6a9473742e91137bf7dab12
ekeneijeoma <ekeneijeoma@351bf6ae-c542-0410-ac58-c1a849201edc>
2010-01-22 23:25:29 +0000

    Added methods for sorting Face objects to Segment class

--- OBJ_Loader/src/saito/objloader/Face.java
+++ OBJ_Loader/src/saito/objloader/Face.java
@@ -10,6 +10,7 @@ package saito.objloader;
  */
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import processing.core.PConstants;
 import processing.core.PVector;
@@ -23,7 +24,7 @@ import processing.core.PVector;
  *         UV's needed to make a face
  */
 
-public class Face implements PConstants {
+public class Face implements PConstants, Comparable {
 
 	public int indexType = POLYGON;
 
@@ -34,7 +35,7 @@ public class Face implements PConstants {
 	public ArrayList<PVector> vertices;
 	public ArrayList<PVector> normals;
 	public ArrayList<PVector> uvs;
-	
+
 	/**
 	 * Constructor for the Element. A model element is all the indices needed to
 	 * draw a face.
@@ -52,15 +53,15 @@ public class Face implements PConstants {
 	public int getIndexCount() {
 		return vertexIndices.size();
 	}
-	
+
 	public int getVertexCount() {
 		return vertices.size();
 	}
-	
+
 	public int getNormalCount() {
 		return normals.size();
 	}
-	
+
 	public int getUVCount() {
 		return uvs.size();
 	}
@@ -111,10 +112,10 @@ public class Face implements PConstants {
 			c.add(vertices.get(i));
 
 		c.div(vertices.size());
-		
+
 		return c;
 	}
-	
+
 	public PVector getNormal() {
 		// center vertex
 		PVector c = getCenter();
@@ -126,7 +127,7 @@ public class Face implements PConstants {
 		PVector n = cToB.cross(aToB);
 
 		n.normalize();
-		
+
 		return n;
 	}
 
@@ -143,10 +144,10 @@ public class Face implements PConstants {
 	public int getNormalIndex(int i) {
 		return normalIndices.get(i) - 1;
 	}
-	
+
 	public boolean isFacingPosition(PVector position) {
 		PVector c = getCenter();
-		
+
 		// this works out the vector from the camera to the face.
 		PVector positionToFace = new PVector(position.x - c.x, position.y - c.y, position.z - c.z);
 
@@ -160,17 +161,54 @@ public class Face implements PConstants {
 		// if the result is positive, then it is facing the camera.
 		return result < 0;
 	}
-	
+
 	public float getFacingAmount(PVector position) {
 		PVector c = getCenter();
-		
+
 		// this works out the vector from the camera to the face.
 		PVector positionToFace = new PVector(position.x - c.x, position.y - c.y, position.z - c.z);
-		
+
 		c.normalize();
-		
+
 		positionToFace.normalize();
-		
-		return (1.0f - (positionToFace.dot(getNormal()) + 1.0f)/2.0f);
+
+		return (1.0f - (positionToFace.dot(getNormal()) + 1.0f) / 2.0f);
 	}
+
+	public int compareTo(Object f2) throws ClassCastException {
+		if (!(f2 instanceof Face))
+			throw new ClassCastException("Face object expected.");
+
+		PVector f1Center = getCenter();
+		PVector f2Center = ((Face) f2).getCenter();
+
+		return (int) (f1Center.x - f2Center.x);
+	}
+
+	public static Comparator<Face> FaceXComparator = new Comparator<Face>() {
+		public int compare(Face f1, Face f2) {
+			PVector f1Center = f1.getCenter();
+			PVector f2Center = f2.getCenter();
+			
+			return (int) (f1Center.x - f2Center.x);
+		}
+	};
+
+	public static Comparator<Face> FaceYComparator = new Comparator<Face>() {
+		public int compare(Face f1, Face f2) {
+			PVector f1Center = f1.getCenter();
+			PVector f2Center = f2.getCenter();
+			
+			return (int) (f1Center.y - f2Center.y);
+		}
+	};
+
+	public static Comparator<Face> FaceZComparator = new Comparator<Face>() {
+		public int compare(Face f1, Face f2) {
+			PVector f1Center = f1.getCenter();
+			PVector f2Center = f2.getCenter();
+			
+			return (int) (f1Center.z - f2Center.z);
+		}
+	};
 }
